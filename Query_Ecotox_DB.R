43ec6f64e20dc1cf7614e1548ea6d9faa3d88594
Tobias Schulze <tobias.schulze@ufz.de>
2022-02-08 15:26:12 +0100

    Update workflow

--- Query_Ecotox_DB.R
+++ Query_Ecotox_DB.R
@@ -14,16 +14,17 @@ set.seed(4711456)
 # Declare the database folder including the current version of EcoToX Knowledgebase
 # A comprehensive name is for example "EcoTox_Fish_EC50"
 
+
 database_path <- "c:/Data/UFZ_DATA/UFZ_Cloud/Databases/Ecotox/current"
 
 # Declare the project folder to store the files of your query
-project_path <- "c:/Data/UFZ_DATA/UFZ_Cloud/Projekte/EcoToxDB/EcoToxDB_Algae_EC10_EC90"
+project_path <- "c:/Data/UFZ_DATA/UFZ_Cloud/Projekte/EcoToxDB/EcoToxDB_Fish_EC10_EC90"
 
 # create the project
 project <- create_project(database_path, project_path,
-                          initalise_database_project = TRUE,
-                          initalise_project = TRUE,
-                          load_default = TRUE)
+                          initalise_database_project = FALSE, # create the basic project from current ASCII files
+                          initalise_project = TRUE, # initializes the project folder
+                          load_default = TRUE) # loads the default project in the project folder
 
 # initialise_database_project (TRUE/FALSE) - create a new basic database out of the EcoTox ASCII files
 # needs to be run each time the ASCII files are updated
@@ -41,37 +42,53 @@ project <- create_project(database_path, project_path,
 
 project <- prepare_data(project = project,
                         habitat = c("Water", "Non-Soil"),
-                        effects = c("MOR", "GRO", "POP"),
+                        effects = c("MOR", "GRO", "POP", "REP", "MPH", "DEV"), # Fish / Crustacean
+                        # effects = c("MOR", "GRO", "POP", "REP"), # Algae
                         save_project = TRUE,
-                        remove_formulation = TRUE)
+                        remove_formulation = FALSE,
+                        new_project_path = NA,
+                        load_initial_project = TRUE)
 
 # Reload the results of the first step
-# load(file.path(project_path,"initial_project.RData"))
+load(file.path(project_path,"initial_project.RData"))
 
 
 
 # Declare species group specific data
 
 # Fish / Crustacean
-measurements = c("MORT", "SURV")
+#measurements = c("MORT", "SURV")
 
 # Algae
 #measurements = c("ABND","APCY","BMAS","CHLC","CHLO","CHLA","DBMS","DWGT","GPOP","GMOR","GGRO","INDX","MORT","PSYN","PSII","PPYT","PGRT","SPGR","SURV","VOLU","WGHT","WWGT")
 
-# Declare the ecotox group
+# Declare the ecotox group ("Species Group")
 #ecotoxgroup = "Algae"
 #ecotoxgroup = "Fish"
-ecotoxgroup = "Crustacean"
+ecotoxgroup = "Fish"
+#ecotoxgroup = "Crustaceans"
+#
 
 
 # Step 2: Filter the data on the specified criteria
 # A list of endpoints and species lists are stored in the project folder for selection
 
-project <- process_data(project, ecotoxgroup = ecotoxgroup,
-                        max_h = 120, max_d = 5,
-                        measurements = measurements)
+project <- process_data(project,
+                        ecotoxgroup = ecotoxgroup,
+                        max_h = 120,
+                        max_d = 5,
+                        kingdoms = c("Chromista","Plantae","Monera"), # valid only for algae
+                        #species_selection = "selected")
+                        #species_selection = "standard_species") # Standardized species only
+                        all_species = TRUE
+)
 
-#load(file = file.path(project_path,paste0(ecotoxgroup,"_state1.RData")))
+
+unique(project$object$mortality_filtered$ecotox_group)
+
+unique(project$object$mortality$endpoint)
+
+#load(file = file.path(project_path, paste0(ecotoxgroup,"_state1.RData")))
 
 # Step 3: Read the modified lists in and process the data including unit conversion
 # A list of chemicals is stored to update missing information on mol weights for data conversion
