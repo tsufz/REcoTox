f89c9dbb86a710d291e61c69016f51f5c1a4f223
Tobias Schulze <tobias.schulze@ufz.de>
2022-09-22 11:31:36 +0200

    Add missing imports

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,12 @@
 #' @importFrom dplyr mutate left_join filter %>% select group_by ungroup
 #' @importFrom dplyr pull arrange bind_rows rowwise if_else
-#' @importFrom dplyr case_when slice_min mutate_all na_if
+#' @importFrom dplyr case_when slice_min mutate_all na_if summarise
 #' @importFrom tibble tibble add_column add_row
 #' @importFrom readr read_delim write_csv read_csv
 #' @importFrom data.table %like%
 #' @importFrom purrr is_empty
 #' @importFrom progress progress_bar
+#' @importFrom tidyr crossing
 #' @importFrom webchem pc_prop
 #'
 .onAttach <- function(libname, pkgname) {
